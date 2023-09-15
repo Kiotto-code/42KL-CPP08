a925d137465ae2ac35ee2556d00bc2a641fe246e
Chan Yik Wai <yichan@student.42kl.edu.my>
2023-09-16 00:52:10 +0800

    done ex01 havent checked

---
+++ ex00/srcs/Easyfind.cpp
@@ -0,0 +1,26 @@
+#include "Easyfind.hpp"
+
+
+
+const char *NotFoundException::what() const throw()
+{
+	return "Huh? What is that?";
+}
+
+int main()
+{
+	std::vector <int> arr;
+	arr.push_back(1);
+	arr.push_back(2);
+	arr.push_back(3);
+	arr.push_back(4);
+	// easyFind(arr, 3);
+	decltype(arr)::iterator iter = easyFind(arr, 3);
+	// std::vector <int>::iterator iter = easyFind(arr, 3);
+	// <decltype (arr)>iterator iter = easyFind(arr, 3);
+	// std::vector<int>::iterator iter = easyFind(arr, 3);
+	std::cout << *iter << std::endl;
+	std::cout << (*iter)++ << std::endl;
+	std::cout << ++(*iter) << std::endl;
+	std::cout << *iter << std::endl;
+}
