dd250c8b1701de8b7ecf1b0a13200eafa76bf667
Chan Yik Wai <86033215+Kiotto-code@users.noreply.github.com>
2024-05-13 14:42:20 +0800

    improved test case

--- ex00/srcs/Easyfind.cpp
+++ ex00/srcs/Easyfind.cpp
@@ -1,11 +1,46 @@
 #include "Easyfind.hpp"
+# include <deque>
 
 // const char *NotFoundException::what() const throw()
 // {
 // 	return "Huh? What is that?";
 // }
 
-int main()
+template<typename Container>
+void printArr( Container &cont)
+{
+    typename Container::iterator ptr;
+
+    for (ptr = cont.begin(); ptr != cont.end(); ptr++)
+        std::cout << *ptr << " ";
+    std::cout << std::endl;
+}
+
+void test_deque(void)
+{
+	try
+	{
+		std::deque <int> arr;
+		arr.push_back(3);
+		arr.push_back(2);
+		arr.push_back(1);
+		arr.push_back(4);
+		// easyFind(arr, 3);
+		decltype(arr)::iterator iter = easyfind(arr, 3);
+		std::cout << std::distance(arr.begin(), iter) << std::endl;
+		iter = easyfind(arr, 4);
+		std::cout << std::distance(arr.begin(), iter) << std::endl;
+		printArr(arr);
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Error:\t" << e.what() << std::endl;
+	}
+}
+
+#include <unistd.h>
+#include <iterator>
+void test_vector()
 {
 	try
 	{
@@ -13,20 +48,26 @@ int main()
 		arr.push_back(3);
 		arr.push_back(2);
 		arr.push_back(1);
+		arr.push_back(1);
 		arr.push_back(4);
 		// easyFind(arr, 3);
 		decltype(arr)::iterator iter = easyfind(arr, 3);
-		// std::vector <int>::iterator iter = easyFind(arr, 3);
-		// <decltype (arr)>iterator iter = easyFind(arr, 3);
-		// std::vector<int>::iterator iter = easyFind(arr, 3);
-		std::cout << *iter << std::endl;
-		std::cout << (*iter)++ << std::endl;
-		std::cout << ++(*iter) << std::endl;
-		std::cout << *iter << std::endl;
+		std::cout << std::distance(arr.begin(), iter) << std::endl;
+		iter = easyfind(arr, 4);
+		std::cout << std::distance(arr.begin(), iter) << std::endl;
+		printArr(arr);
 	}
 	catch (const std::exception &e)
 	{
 		std::cout << "Error:\t" << e.what() << std::endl;
 	}
+}
+
+int main()
+{
+	std::cout << std::uppercase << "test_vector" << std::endl;
+	test_vector();
+	std::cout << std::uppercase << "test_deque" << std::endl;
+	test_deque();
 }
 
